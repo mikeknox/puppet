3a029837d2331301440871ea8a04d151ea9ad40d
Markus Roberts <Markus@reality.com>
2010-04-30 16:39:08 -0700

    Change the main spec to an apply spec
    This appears to have been left out of the preceeding commit sequence.

--- spec/integration/application/apply.rb
+++ spec/integration/application/apply.rb
@@ -4,9 +4,9 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 require 'puppet_spec/files'
 
-require 'puppet/application/main'
+require 'puppet/application/agent'
 
-describe "main" do
+describe "agent" do
     include PuppetSpec::Files
 
     describe "when applying provided catalogs" do
@@ -21,7 +21,7 @@ describe "main" do
 
             File.open(manifest, "w") { |f| f.print catalog.to_pson }
 
-            puppet = Puppet::Application[:main]
+            puppet = Puppet::Application[:agent]
             puppet.options[:catalog] = manifest
 
             puppet.apply
